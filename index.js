5ee081becf9bdc960ccc9c8acbecfb768cdd9af6
Kavya Katal <kavyakatal09@gmail.com>
2025-08-21 17:29:03 +0530

    figlet example

--- index.js
+++ index.js
@@ -15,3 +15,22 @@ console.log(random())
 // Figlet
 
 const figlet = require("figlet")
+
+figlet.text(
+  "Boo!",
+  {
+    font: "Ghost",
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 80,
+    whitespaceBreak: true,
+  },
+  function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  }
+);
